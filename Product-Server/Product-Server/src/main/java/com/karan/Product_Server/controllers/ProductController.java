6836d1e5ecef71e765459b129c0fbbe8c73ec796
Soumyadip Karan <soumyadipkaran65@gmail.com>
2025-12-04 01:52:34 +0530

    Load Balancer

--- Product-Server/Product-Server/src/main/java/com/karan/Product_Server/controllers/ProductController.java
+++ Product-Server/Product-Server/src/main/java/com/karan/Product_Server/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.karan.Product_Server.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/product")
     public String placeOrder(){
-        return "Order placed successfully";
+
+        String port = env.getProperty("server.port");
+        return "Order placed successfully :: Running on "+port+" Port";
     }
 
     @GetMapping("/product/{name}")
